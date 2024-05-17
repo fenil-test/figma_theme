538d42506b2aa635d2cb9702f96ac786599bd0ff
fenil-test <169125616+fenil-test@users.noreply.github.com>
2024-05-17 18:03:19 +0530

    javascriptr running..

---
+++ assets/size-guide.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+    $("#unique_text_QDhCha").click(function(){
+        console.log("fa");
+        $(".size_guide__box").show();
+    });
+    $("#unique_text_QDhCha").click(function(){
+        $(".size_guide__box").hide();
+    });
+});
